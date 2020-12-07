d42cd07fd3145770ed2b0673787f51ceb4d9ff5b
soulr344 <bloodthirstyoff@gmail.com>
2020-12-07 22:44:10 +0545

    requirements: bruh momento
    Signed-off-by: soulr344 <bloodthirstyoff@gmail.com>

--- init.sh
+++ init.sh
@@ -5,6 +5,6 @@ sudo apt-get update && sudo apt-get install git neofetch ffmpeg flac net-tools n
 sudo apt-get install python3.8 python3-pip &> /dev/null
 sudo apt-get install python3-setuptools &> /dev/null
 echo "Installing python requirements..."
-python3 -m pip install wheel &> /dev/null
-python3 -m pip install -r requirements.txt
+python3.8 -m pip install wheel &> /dev/null
+python3.8 -m pip install -r requirements.txt
 echo "Running the userbot.."
